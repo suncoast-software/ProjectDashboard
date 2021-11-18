4bb2202ae193c7a93b45b058eb1c863a1b37f79a
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2021-11-18 04:37:45 -0500

    add global usings .Net 6

--- Dashboard.Core/App.xaml.cs
+++ Dashboard.Core/App.xaml.cs
@@ -1,41 +1,30 @@
-﻿using Dashboard.Core.ViewModels;
-using Dashboard.Data.Factories;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿namespace Dashboard.Core;
 
-namespace Dashboard.Core
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private readonly IServiceProvider _serviceProvider;
+
+    public App()
     {
-        private readonly IServiceProvider _serviceProvider;
-
-        public App()
+        IServiceCollection services = new ServiceCollection();
+        services.AddTransient<DbContextFactory>();
+        services.AddSingleton<AppViewModel>();
+        services.AddSingleton<MainWindow>(s => new MainWindow()
         {
-            IServiceCollection services = new ServiceCollection();
-            services.AddTransient<DbContextFactory>();
-            services.AddSingleton<AppViewModel>();
-            services.AddSingleton<MainWindow>(s => new MainWindow()
-            {
-                DataContext = s.GetRequiredService<AppViewModel>()
-            });
+            DataContext = s.GetRequiredService<AppViewModel>()
+        });
 
-            _serviceProvider = services.BuildServiceProvider();
-        }
+        _serviceProvider = services.BuildServiceProvider();
+    }
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            MainWindow = _serviceProvider.GetService<MainWindow>();
-            MainWindow?.Show();
-            base.OnStartup(e);
-        }
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainWindow = _serviceProvider.GetService<MainWindow>();
+        MainWindow?.Show();
+        base.OnStartup(e);
     }
 }
+
